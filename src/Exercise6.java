2ed54a6a55741fd522be1b9b696596cb2bda739c
JunJun435 <50116354+JunJun435@users.noreply.github.com>
2020-10-02 14:07:33 -0400

    wow i can't even spell exercise correctly

--- src/Exercise6.java
+++ src/Exercise6.java
@@ -1,7 +1,7 @@
 import java.text.DecimalFormat;
 import java.util.Scanner;
 
-public class Excercise6 {
+public class Exercise6 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
